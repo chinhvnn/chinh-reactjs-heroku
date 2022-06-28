19739c552d8aa1a4a52d64a34e829e8aa9f99847
unknown <chinhqn09@gmail.com>
2022-06-28 17:44:15 +0700

    todolist2-change server

--- src/untils/axiosClient.js
+++ src/untils/axiosClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const port = process.env.PORT || '3000'
+const port = process.env.PORT || '3001'
 
 const axiosClient = axios.create({
   baseURL: process.env.REACT_APP_BASE_BE+port,
